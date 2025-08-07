a73c514c8db09142c596307268a774a368dd8e2d
Jakub Górski <jakubg1527@gmail.com>
2025-08-07 21:29:35 +0200

    Add auto formatters install

---
+++ lua/plugins/mason-tool-installer.lua
@@ -0,0 +1,11 @@
+return {
+  "WhoIsSethDaniel/mason-tool-installer.nvim",
+  opts = {
+    ensure_installed = {
+      "stylua", -- Lua
+      "black", -- Python
+      "prettier", -- JS/TS/CSS/HTML
+      "clang-format", -- C/C++
+    },
+  },
+}
