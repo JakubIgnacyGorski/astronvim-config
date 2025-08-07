0d97f68ae8af6b0fb89c83d2e90601a045ed01c7
Jakub Górski <jakubg1527@gmail.com>
2025-08-07 21:49:41 +0200

    Add separate file for spell checker

--- init.lua
+++ init.lua
@@ -25,8 +25,4 @@ end
 
 require "lazy_setup"
 require "polish"
-
--- Add spell checker
-vim.opt.spell = true
--- When there is lack of .spl file download it from there: https://ftp.nluug.nl/vim/runtime/spell/ and place it in: ~/.local/share/nvim/site/spell/
-vim.opt.spelllang = { 'pl', 'en' }
+require "user.spell-checker"
