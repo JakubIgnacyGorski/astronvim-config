fd716c1d2646443ab2f3d1e189c6f0ae96c1646a
Jakub Górski <jakubg1527@proton.me>
2025-08-13 19:05:17 +0200

    Add function actions and tab buffer switching

--- lua/plugins/mappings.lua
+++ lua/plugins/mappings.lua
@@ -9,6 +9,10 @@ return {
           -- this is useful for naming menus
           ["<Leader>b"] = { desc = "Buffers" },
           ["<C-n>"] = { "<cmd>Neotree toggle<cr>", desc = "Toggle Neotree" },
+
+          -- buffer operations
+          ["<Tab>"] = { function() require("astrocore.buffer").nav(vim.v.count1) end, desc = "Next buffer" },
+          ["<S-Tab>"] = { function() require("astrocore.buffer").nav(-vim.v.count1) end, desc = "Previous buffer" },
         },
       },
     },
@@ -24,11 +28,17 @@ return {
             function() vim.lsp.buf.hover() end,
             desc = "Hover symbol details",
           },
-          -- condition for only server with declaration capabilities
           gD = {
             function() vim.lsp.buf.declaration() end,
-            desc = "Declaration of current symbol",
-            cond = "textDocument/declaration",
+            desc = "Go to declaration of current symbol",
+          },
+          gd = {
+            function() vim.lsp.buf.definition() end,
+            desc = "Go to definition of current symbol",
+          },
+          ["<Leader>lc"] = {
+            function() vim.lsp.buf.references() end,
+            desc = "List references (calls to) current symbol",
           },
           ["<Leader>r"] = { desc = "Renames" },
           ["<Leader>rn"] = { function() vim.lsp.buf.rename() end, desc = "Rename symbol" },
