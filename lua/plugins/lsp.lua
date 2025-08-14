d7bbea95e9cfd4ad09d4bfd950196bb0a3ef4938
Jakub Górski <jakubg1527@proton.me>
2025-08-14 20:43:18 +0200

    Add rust support - use of rust commands FROM SYSTEM

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -4,15 +4,13 @@ return {
   {
     "williamboman/mason.nvim",
     build = ":MasonUpdate",
-    config = function()
-      require("mason").setup()
-    end,
+    config = function() require("mason").setup() end,
   },
   {
     "williamboman/mason-lspconfig.nvim",
     dependencies = { "williamboman/mason.nvim" },
     config = function()
-      require("mason-lspconfig").setup({
+      require("mason-lspconfig").setup {
         ensure_installed = {
           "pyright",
           "clangd",
@@ -21,18 +19,36 @@ return {
           "jsonls",
         },
         automatic_installation = true,
-      })
+      }
     end,
   },
   {
     "neovim/nvim-lspconfig",
     config = function()
-      local lspconfig = require("lspconfig")
-      lspconfig.pyright.setup({})
-      lspconfig.clangd.setup({})
-      lspconfig.lua_ls.setup({})
-      lspconfig.bashls.setup({})
-      lspconfig.jsonls.setup({})
+      local lspconfig = require "lspconfig"
+      lspconfig.pyright.setup {}
+      lspconfig.clangd.setup {}
+      lspconfig.lua_ls.setup {}
+      lspconfig.bashls.setup {}
+      lspconfig.jsonls.setup {}
+      lspconfig.rust_analyzer.setup {
+        cmd = { "rust-analyzer" },
+        settings = {
+          ["rust-analyzer"] = {
+            files = {
+              excludeDirs = {
+                ".direnv",
+                ".git",
+                "target",
+              },
+            },
+            check = {
+              command = "clippy",
+              extraArgs = { "--no-deps" },
+            },
+          },
+        },
+      }
     end,
   },
 }
