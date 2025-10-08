b4056856efbefe6c6cca006e64eb9adbd03cb2d8
Jakub Górski <jakubg1527@proton.me>
2025-10-08 09:00:00 +0200

    Add copilot chat and function to turn off copilot suggestions

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -26,7 +26,6 @@ return {
       opts = {
         options = {
           g = {
-            -- set the ai_accept function
             ai_accept = function()
               if require("copilot.suggestion").is_visible() then
                 require("copilot.suggestion").accept()
@@ -37,7 +36,10 @@ return {
         },
         mappings = {
           n = {
-            ["grc"] = { "<cmd>Copilot suggestion toggle_auto_trigger<cr>", desc = "Toggle Copilot suggestion" },
+            ["<leader>ms"] = {
+              function() require("copilot.suggestion").toggle_auto_trigger() end,
+              desc = "Toggle Copilot suggestion",
+            },
           },
         },
       },
